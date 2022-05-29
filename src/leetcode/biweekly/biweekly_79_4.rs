4375c6665c2ccbb87a0979c00b4a780f1914c835
Xana Hopper <xanarust@gmail.com>
2022-05-29 10:28:13 +0800

    biweekly_79

--- src/leetcode/biweekly/biweekly_79_4.rs
+++ src/leetcode/biweekly/biweekly_79_4.rs
@@ -18,31 +18,31 @@ impl BookMyShow {
     }
 
     fn gather(&mut self, k: i32, max_row: i32) -> Vec<i32> {
-        for (index, mut row) in self.rows[0..=max_row as usize].iter().enumerate() {
-            if let Some(position) = row.iter().enumerate().filter(|r| r.len() >= k as usize).min_by_key(|r| r.1.len()) {
-                let r = row.remove(position.0);
-                if r.len() > k as usize {
-                    row.push(r.start + k .. r.end)
-                }
-                return vec![index as i32, r.start]
-            }
-        }
+        // for (index, mut row) in self.rows[0..=max_row as usize].iter().enumerate() {
+        //     if let Some(position) = row.iter().enumerate().filter(|r| r.len() >= k as usize).min_by_key(|r| r.1.len()) {
+        //         let r = row.remove(position.0);
+        //         if r.len() > k as usize {
+        //             row.push(r.start + k .. r.end)
+        //         }
+        //         return vec![index as i32, r.start]
+        //     }
+        // }
         vec![]
     }
 
     fn scatter(&mut self, k: i32, max_row: i32) -> bool {
-        if self.rows[0..=max_row as usize].iter().fold(0, |res, r| {
-            res + r.iter().map(|x| x.len()).sum()
-        }) >= k {
-            // self.rows[0..=max_row as usize].iter().enumerate().fold((true, k), |(res, remain), (index, range)| {
-            //     if let Some((position, r)) = range.iter().enumerate().min_by_key(|r| r.1.len()) {
-            //         if r.len() > remain as usize {
-            //
-            //         }
-            //     }
-            // });
-            true
-        } else { false }
+        // if self.rows[0..=max_row as usize].iter().fold(0, |res, r| {
+        //     res + r.iter().map(|x| x.len()).sum()
+        // }) >= k {
+        //     // self.rows[0..=max_row as usize].iter().enumerate().fold((true, k), |(res, remain), (index, range)| {
+        //     //     if let Some((position, r)) = range.iter().enumerate().min_by_key(|r| r.1.len()) {
+        //     //         if r.len() > remain as usize {
+        //     //
+        //     //         }
+        //     //     }
+        //     // });
+        //     true
+        // } else { false }
 
         // for (index, mut row) in self.rows[0..=max_row as usize].iter().enumerate() {
         //     if let Some(position) = row.iter().enumerate().min_by_key(|r| r.1.len()) {
